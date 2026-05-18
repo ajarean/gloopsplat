d9fb5f9274738b4ae161295f8aa7f6f35b4d4608
tingtingtingtin <twu062604@gmail.com>
2026-05-18 14:59:38 -0700

    feat: instanced rendering

--- shaders/splat.vs
+++ shaders/splat.vs
@@ -1,15 +1,16 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 position;
+layout (location = 2) in float radius;
+layout (location = 3) in vec4 color;
+layout (location = 4) in vec3 normal;
+layout (location = 5) in float speed;
 
 uniform mat4 projection;
 uniform mat4 modelView;
 uniform vec2 viewport;
 uniform vec2 focal;
-uniform vec3 position;
-uniform float radius;
-uniform vec4 color;
-uniform vec3 normal;
 
 out vec4 vColor;
 out vec3 vNormal;
--- src/splatRenderer.cpp
+++ src/splatRenderer.cpp
@@ -13,6 +13,7 @@ SplatRenderer::SplatRenderer() {
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
   glGenBuffers(1, &EBO);
+  glGenBuffers(1, &splatVBO);
 
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -22,28 +23,49 @@ SplatRenderer::SplatRenderer() {
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, splatVBO);
+  glBufferData(GL_ARRAY_BUFFER, 0, nullptr, GL_DYNAMIC_DRAW);
+
+  // position (vec3)
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(SplatData), (void*)offsetof(SplatData, position));
+  glEnableVertexAttribArray(1);
+  glVertexAttribDivisor(1, 1);
+
+  // radius (float)
+  glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(SplatData), (void*)offsetof(SplatData, radius));
+  glEnableVertexAttribArray(2);
+  glVertexAttribDivisor(2, 1);
+
+  // color (vec4)
+  glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(SplatData), (void*)offsetof(SplatData, color));
+  glEnableVertexAttribArray(3);
+  glVertexAttribDivisor(3, 1);
+
+  // normal (vec3)
+  glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(SplatData), (void*)offsetof(SplatData, normal));
+  glEnableVertexAttribArray(4);
+  glVertexAttribDivisor(4, 1);
+
+  // speed (float)
+  glVertexAttribPointer(5, 1, GL_FLOAT, GL_FALSE, sizeof(SplatData), (void*)offsetof(SplatData, speed));
+  glEnableVertexAttribArray(5);
+  glVertexAttribDivisor(5, 1);
   glBindVertexArray(0);
 }
 
 void SplatRenderer::draw(Shader &shader, std::vector<Particle> particles, glm::mat4 &projection,
     glm::mat4 &modelView, glm::vec2 &focal, glm::vec2 &viewport) {
-  // shader.use();
-  // uniform per gaussian
+  depthSort(particles, modelView);
+  updateBuffers(particles);
+
   shader.setMat4("projection", projection);
   shader.setMat4("modelView", modelView);
   shader.setVec2("focal", focal);
   shader.setVec2("viewport", viewport);
 
   glBindVertexArray(VAO);
-  depthSort(particles, modelView);
-  for (int i : indices) {
-    Particle p = particles[i];
-    shader.setVec3("position", p.position);
-    shader.setVec4("color", p.color);
-    shader.setVec3("normal", p.normal);
-    shader.setFloat("radius", p.radius);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-  }
+  glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0, particles.size());
   glBindVertexArray(0);
 }
 
@@ -60,6 +82,20 @@ void SplatRenderer::depthSort(const std::vector<Particle>& particles, const glm:
     float z_b = (modelView*glm::vec4(particles[b].position, 1.0f)).z;
     return z_a < z_b; // -z is more in front in opengl
   });
+}
+
+void SplatRenderer::updateBuffers(const std::vector<Particle> particles) {
+  splatBuffer.resize(particles.size());
+  for (int i = 0; i < splatBuffer.size(); i++) {
+    const Particle& p = particles[indices[i]];
+    splatBuffer[i].position = p.position;
+    splatBuffer[i].radius = p.radius;
+    splatBuffer[i].color = p.color;
+    splatBuffer[i].normal = p.normal;
+    splatBuffer[i].speed = glm::length(p.velocity);
+  }
+  glBindBuffer(GL_ARRAY_BUFFER, splatVBO);
+  glBufferData(GL_ARRAY_BUFFER, splatBuffer.size() * sizeof(SplatData), splatBuffer.data(), GL_DYNAMIC_DRAW);
 }
 
 SplatRenderer::~SplatRenderer() {
